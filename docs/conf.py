6fffed04b648736046e4ffee0a0cb3999e7c4147
Aditya Penumarti <94308240+adipenu@users.noreply.github.com>
2022-06-03 15:49:51 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.graphviz', 'recommonmark']
+extensions = ['sphinx.ext.viewcode', 'sphinx.ext.graphviz', 'recommonmark','sphinx_copybutton']
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -59,11 +59,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-
-
-
-extensions = [
-    ...
-    'sphinx_copybutton'
-    ...
-]
